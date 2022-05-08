aae8a7d1cf9f938ea05d3a0e2ab8f6b238744445
Andres Ortiz <3283@holbertonschool.com>
2022-05-08 02:01:13 -0300

    Implemented FAB with Click Listener but no action yet

--- app/src/main/java/com/example/parkingapp/MainActivity.kt
+++ app/src/main/java/com/example/parkingapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.parkingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.parkingapp.R
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab: View = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Snackbar!!", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
 }
